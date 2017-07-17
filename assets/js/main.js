026ea33af953b474994cbdd2e3e02a5d6f204d18
cstapiarubio <cstapiarubio@gmail.com>
2017-07-16 20:13:39 -0400

    css nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,22 @@
 /*funcion ver los datos del pokemon buscado*/
 $(document).ready(function(){
-    $("#botonVerPoke").on("click", function(){
-        var nombrePoke = $("#nombre").val();
-    $.ajax({
-            url: 'http://pokeapi.co/api/v1/pokemon/' + nombrePoke,
-            type: 'GET',
-            datatype: 'JSON',
-            
-       })
+	$("#botonVerPoke").on("click", function(){
+		var nombrePoke = $("#nombre").val();
+		$.ajax({
+			url: 'http://pokeapi.co/api/v1/pokemon/' + nombrePoke,
+			type: 'GET',
+			datatype: 'JSON',
+			
+		})
 
-       .done(function(response){
+		.done(function(response){
              //div vacio//
-        $("#contenedorPoke").append("<div id=''>"+'<p>Pokemon</p>'+ response.name  + response.abilities[0].name + response.abilities[1].name + response.types[0].name + response.types[1].name+  response.height + response.weight +"</div>")
-            console.log(response.name);
-        })
+             $("#contenedorPoke").append("<div id=''>"+'<p>Pokemon</p>'+ response.name  + response.abilities[0].name + response.abilities[1].name + response.types[0].name + response.types[1].name+  response.height + response.weight +"</div>")
+             console.log(response.name);
+         })
 
-       .fail(function(error){
-            console.log("error");
-        })
-    });
-    })
+		.fail(function(error){
+			console.log("error");
+		})
+	});
+})
